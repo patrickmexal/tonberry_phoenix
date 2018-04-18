bbbe06650b403bc2d6743d7f8375f84d8539c54c
Patrick Mexal <patrickmexal@gmail.com>
2018-04-17 21:07:14 -0700

    added some other stuff need to figure out app.js and App.js on Wednesday

--- tonberry-flight/src/App.js
+++ tonberry-flight/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from "axios";
 import Login from "./components/Login.js";
 import Register from "./components/Register.js";
 import Create from "./components/Create.js";
@@ -8,6 +9,28 @@ import Header from "./components/Header.js";
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    };
+  }
+
+  componentDidMount() {
+    axios.defaults.headers.common['Authorization'] = localStorage.getItem('jwtToken');
+    axios.get('/api/book')
+      .then(res => {
+        this.setState({ books: res.data });
+        console.log(this.state.books);
+      })
+      .catch((error) => {
+        if(error.response.status === 401) {
+          this.props.history.push("/login");
+        }
+      });
+  }
+
   render() {
     return (
       <div>
